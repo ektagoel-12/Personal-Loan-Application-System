64ba14d3625f700d465f9b8070904abd1b2a8233
aom <wankhede.aom@gmail.com>
2025-09-24 12:27:50 +0530

    jwt auth added

--- backend/loan-application/src/main/java/tech/zetapioneers/loan_application/LoanApplication.java
+++ backend/loan-application/src/main/java/tech/zetapioneers/loan_application/LoanApplication.java
@@ -2,12 +2,13 @@ package tech.zetapioneers.loan_application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class LoanApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(LoanApplication.class, args);
 	}
-
 }
---
+++ backend/loan-application/src/main/java/tech/zetapioneers/loan_application/controllers/UserController.java
@@ -0,0 +1,20 @@
+package tech.zetapioneers.loan_application.controllers;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @GetMapping
+    public String sccee(){
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null && auth.isAuthenticated()) {
+            return auth.getPrincipal().toString(); // in our filter, we set principal = user ID
+        }
+        return null;
+    }
+}
