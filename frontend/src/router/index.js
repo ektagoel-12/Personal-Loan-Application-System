71a1fe8fb84369dda66e8d00e5534880cdbdabb1
A4nchal <aanchal20030523@gmail.com>
2025-09-24 15:20:19 +0530

    Landing Page

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -7,6 +7,8 @@ import AdminTicketView from '@/views/AdminTicketView.vue'
 import UserTicketsView from '@/views/UserTicketsView.vue'
 import EmiCalculator from '@/views/EmiCalculator.vue'
 import AdminDashboard from '../views/AdminDashboard.vue'
+import LandingPage from '../views/LandingPage.vue'
+
 
 import { createRouter, createWebHistory } from 'vue-router'
 
@@ -14,9 +16,10 @@ import { createRouter, createWebHistory } from 'vue-router'
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    {path:'/',component:HomeDashboard,name:"HomeDashboard"},
-    {path:'/login-form',component:UserLoginForm,name:"UserLoginForm"},
-    {path:'/registration-form',component:UserRegistrationForm,name:"UserRegistrationForm"},
+    { path: '/', component: LandingPage, name: "LandingPage" },
+    { path: '/user-dashboard', component: HomeDashboard, name: "HomeDashboard" },
+    { path: '/login-form', component: UserLoginForm, name: "UserLoginForm" },
+    { path:'/registration-form',component:UserRegistrationForm,name:"UserRegistrationForm"},
     { path : '/loan' , component : LoanComponent},
     {path: '/raise-ticket',component: RaiseTicketView},
     {path: '/admin-ticket-view', component:AdminTicketView},
