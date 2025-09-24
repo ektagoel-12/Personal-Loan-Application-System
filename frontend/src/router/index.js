dca051c1b3bf0af3fce2e00efb76706824dd2809
Premansu <premansuchakraborty02@gmail.com>
2025-09-24 10:05:41 +0530

    support ticket components added

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,6 +3,9 @@ import UserLoginForm from '@/views/UserLoginForm.vue'
 import UserRegistrationForm from '@/views/UserRegistrationForm.vue'
 import LoanComponent from '@/views/LoanComponent.vue'
 import { createRouter, createWebHistory } from 'vue-router'
+import RaiseTicketView from '@/views/RaiseTicketView.vue'
+import AdminTicketView from '@/views/AdminTicketView.vue'
+import UserTicketsView from '@/views/UserTicketsView.vue'
 
 const router = createRouter({
   history: createWebHistory(),
@@ -10,7 +13,10 @@ const router = createRouter({
     {path:'/',component:HomeDashboard,name:"HomeDashboard"},
     {path:'/login-form',component:UserLoginForm,name:"UserLoginForm"},
     {path:'/registration-form',component:UserRegistrationForm,name:"UserRegistrationForm"},
-    { path : '/loan' , component : LoanComponent}
+    { path : '/loan' , component : LoanComponent},
+    {path: '/raise-ticket',component: RaiseTicketView},
+    {path: '/admin-ticket-view', component:AdminTicketView},
+    {path: '/user-ticket-view', component: UserTicketsView}
   ],
 })
 
