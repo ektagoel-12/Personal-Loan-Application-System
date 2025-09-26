bda1c24f5a8373cc63bf8949a899ad5eb7866317
A4nchal <aanchal20030523@gmail.com>
2025-09-26 20:02:15 +0530

    Resolved cors conflict

--- backend/loan-application/src/main/java/tech/zetapioneers/loan_application/config/CorsConfig.java
+++ backend/loan-application/src/main/java/tech/zetapioneers/loan_application/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of("http://localhost:5173")); // your frontend
-        configuration.setAllowedMethods(List.of("GET", "POST", "PATCH", "DELETE", "OPTIONS"));
+        configuration.setAllowedMethods(List.of("GET", "POST","PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
 
